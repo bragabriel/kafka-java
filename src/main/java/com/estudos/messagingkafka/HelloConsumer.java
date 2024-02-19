b8a01e87421d4d42a41b5c6d08f3926c753cc290
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2024-02-19 16:37:18 -0300

    feat: add Consumer and Producer

--- src/main/java/com/estudos/messagingkafka/HelloConsumer.java
+++ src/main/java/com/estudos/messagingkafka/HelloConsumer.java
@@ -1,2 +1,13 @@
-package com.estudos.messagingkafka;public class HelloConsumir {
+package com.estudos.messagingkafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloConsumer {
+
+    @KafkaListener(topics = "hello-topic", groupId = "group-1")
+    public void receiveMessage(String message){
+        System.out.println("Consumer Message: " + message);
+    }
 }
--- src/main/java/com/estudos/messagingkafka/HelloProducer.java
+++ src/main/java/com/estudos/messagingkafka/HelloProducer.java
@@ -1,2 +1,19 @@
-package com.estudos.messagingkafka;public class HelloProducer {
+package com.estudos.messagingkafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloProducer {
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+    //              <mensagem, tópico>
+
+    public HelloProducer(KafkaTemplate<String, String> kafkaTemplate){
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message){
+        kafkaTemplate.send("hello-topic", message);
+    }
 }
