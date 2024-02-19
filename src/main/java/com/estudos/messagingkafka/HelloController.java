80c87ba6733f24e282437dc2200fa961c4ad4bad
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2024-02-19 16:37:25 -0300

    feat: add Controller

--- src/main/java/com/estudos/messagingkafka/HelloController.java
+++ src/main/java/com/estudos/messagingkafka/HelloController.java
@@ -1,2 +1,26 @@
-package com.estudos.messagingkafka;public class HelloController {
+package com.estudos.messagingkafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+public class HelloController{
+
+    @Autowired
+    private HelloProducer helloProducer;
+
+    @GetMapping("/hello/{name}")
+    public ResponseEntity hello(@PathVariable("name") String name){
+    helloProducer.sendMessage("Hello " + name);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+
 }
